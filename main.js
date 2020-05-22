ed2042e9c5d637df2d81aa656dfdc6e0307009af
B <blytheballesteros@lclark.edu>
2020-05-21 16:08:46 -1000

    added sections and linked menu to add to specific list

--- main.js
+++ main.js
@@ -18,12 +18,7 @@ for (i = 0; i < close.length; i++) {
     div.style.display = "none";
   }
 }
-if (close.length = 0){
-  var p = document.createElement("p");
-  var txt = document.createTextNode("You have no task to do");
-  p.appendChild(txt);
-  document.getElementById("myUL").appendChild(p)
-}
+
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
@@ -36,24 +31,82 @@ list.addEventListener('click', function(ev) {
 
 
 
-// Create a new list item when clicking on the "Add" button
-function newElement() {
+// Create a new must item when clicking on the "Add" button
+function newMust() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
-  var div = document.getElementById("myUL");
-  if (inputValue === '') {
-    alert("You must write something!");
-  }
-  else if(div.getElementsByTagName("p").length == 1) {
-    var element = document.getElementById("empty");
+  var div = document.getElementById("mustUL");
+  if(div.getElementsByTagName("p").length == 1) {
+    var element = document.getElementById("emptymust");
     element.remove();
-    document.getElementById("myUL").appendChild(li);
-
+    div.appendChild(li);
   }
   else{
-    document.getElementById("myUL").appendChild(li);
+    div.appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+// Create a new should item when clicking on the "Add" button
+function newShould() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  var div = document.getElementById("shouldUL");
+  if(div.getElementsByTagName("p").length == 1) {
+    var element = document.getElementById("emptyshould");
+    element.remove();
+    div.appendChild(li);
+  }
+  else{
+    div.appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+// Create a new could item when clicking on the "Add" button
+function newCould() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  var div = document.getElementById("couldUL");
+  if(div.getElementsByTagName("p").length == 1) {
+    var element = document.getElementById("emptycould");
+    element.remove();
+    div.appendChild(li);
+  }
+  else{
+    div.appendChild(li);
   }
   document.getElementById("myInput").value = "";
 
@@ -75,7 +128,12 @@ function newElement() {
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
+  var inputValue = document.getElementById("myInput").value;
+  if (inputValue === '') {
+    alert("You must write something!");
+  }else{
   document.getElementById("myDropdown").classList.toggle("show");
+  }
 }
 
 // Close the dropdown menu if the user clicks outside of it
